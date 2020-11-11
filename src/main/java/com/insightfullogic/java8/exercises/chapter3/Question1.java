c9f77a43ebe3095b527da9618bbd82983c4e4c49
Christian Capito <ccapito@kombiverkehr.de>
2020-11-11 17:23:23 +0100

    Chapter 3 excercises

--- src/main/java/com/insightfullogic/java8/exercises/chapter3/Question1.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter3/Question1.java
@@ -11,14 +11,18 @@ import static java.util.stream.Collectors.toList;
 
 public class Question1 {
     public static int addUp(Stream<Integer> numbers) {
-        return Exercises.replaceThisWithSolution();
+        return numbers.reduce(0, (acc, element) -> acc + element);
     }
 
     public static List<String> getNamesAndOrigins(List<Artist> artists) {
-        return Exercises.replaceThisWithSolution();
+        return artists.stream()
+            .flatMap(artist -> Stream.of(artist.getName(), artist.getNationality()))
+            .collect(toList());
     }
 
     public static List<Album> getAlbumsWithAtMostThreeTracks(List<Album> input) {
-        return Exercises.replaceThisWithSolution();
+        return input.stream()
+            .filter(album -> album.getTrackList().size() <= 3)
+            .collect(toList());
     }
 }
